e72b6bb37ce79e11aaec0ec17a53150ab2993f47
Anton Demidenko <yaintroversivnost@gmail.com>
2024-05-22 16:02:39 +0500

    Исправление страницы обработок ошибок

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<!-- обрабатываем ошибки страницы -->
+<h1>{$page.status}</h1>
+<p>Хм, страницы <code>{$page.url.pathname}</code> нет</p>
+
+<style lang="scss">
+	code {
+		padding: 5px 10px;
+		background-color: rgb(36, 36, 36);
+		color: #fff;
+		border-radius: 5px;
+	}
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: '404.html',
+			fallback: undefined,
 			precompress: false,
 			strict: true
 		})
