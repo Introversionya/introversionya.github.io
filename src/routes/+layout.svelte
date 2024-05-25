fcdab653b74317def1fcc1eede1e9eea68174881
Anton Demidenko <yaintroversivnost@gmail.com>
2024-05-25 16:49:15 +0500

    Тестирование автопроверки нового контента

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
 <script>
 	import "../assets/scss/global.scss"
+	import { updated } from '$app/stores';
 </script>
 
 <slot />
+
+{#if $updated}
+  <div class="toast">
+    <p>
+      A new version of the app is available
+      <button on:click={() => location.reload()}>
+        reload the page
+      </button>
+    </p>
+  </div>
+{/if}
+
+<button on:click={() => updated.check()}>
+	проверить
+</button>
--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,16 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	preprocess: preprocess(),
 	kit: {
-		adapter: htmlMinifier(adapter({pages: 'build',assets: 'build',fallback: '404.html',precompress: false,strict: true}), {collapseWhitespace: true,removeComments: true})
+		adapter: htmlMinifier(adapter({pages: 'build',assets: 'build',fallback: '404.html',precompress: false,strict: true}), {collapseWhitespace: true,removeComments: true}),
+
+		version: {
+			// ideally, this should be something deterministic
+			// like the output of `git rev-parse HEAD`
+			name: Date.now().toString(),
+
+			// if undefined, no polling will occur
+			pollInterval: 5000
+		}
 	}
 };
 
