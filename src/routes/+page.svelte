ec4fc85958b66de16d59e75c2866818e2a4486ac
Anton Demidenko <yaintroversivnost@gmail.com>
2024-05-25 16:51:04 +0500

    тестовое обновление контента

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,6 @@
 	<title>{title}</title>
 </svelte:head>
 
-<h1>home</h1>
+<h1>home!</h1>
 <a href="/about">about</a>
 <button class="test" on:click={() => counter++}>counter++ | count: {counter}</button>
