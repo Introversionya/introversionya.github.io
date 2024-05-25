667f03a8a5665254b9614aba24c857e0a03a5d93
Anton Demidenko <yaintroversivnost@gmail.com>
2024-05-25 14:15:38 +0500

    Настройка минификации build

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
+import htmlMinifier from 'sveltekit-html-minifier';
 
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: true
-		})
+		adapter: htmlMinifier(adapter({pages: 'build',assets: 'build',fallback: '404.html',precompress: false,strict: true}), {collapseWhitespace: true,removeComments: true})
 	}
 };
 
