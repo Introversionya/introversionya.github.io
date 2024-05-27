140d4890e1bf74b545abd6479e8533e05d61e923
Anton Demidenko <yaintroversivnost@gmail.com>
2024-05-27 06:29:50 +0500

    Базовая структура scss

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,16 +1,11 @@
+<!-- обрабатываем ошибки -->
 <script>
-	import { page } from '$app/stores';
+	import { page } from "$app/stores";
 </script>
 
-<!-- обрабатываем ошибки -->
-<h1>{$page.status}</h1>
-<p>Хм, страницы <code>{$page.url.pathname}</code> нет</p>
+<main>
+	<h1>Ошибка: {$page.status}</h1>
+	<a href="/">Вернуться на главную</a>
+	<button on:click={() =>location.reload()}>Перезагрузить страницу</button>
+</main>
 
-<style lang="scss">
-	code {
-		padding: 5px 10px;
-		background-color: rgb(36, 36, 36);
-		color: #fff;
-		border-radius: 5px;
-	}
-</style>
