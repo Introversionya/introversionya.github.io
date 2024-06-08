06fd423ac117d7dffd3325360ad7c2ed66a3f019
Anton Demidenko <yaintroversivnost@gmail.com>
2024-06-08 09:04:51 +0500

    Обновление gitignore

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/entries/pages/_layout.server.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/entries/pages/_layout.server.js
@@ -3,7 +3,6 @@ const ssr = true;
 const csr = true;
 function load({ setHeaders }) {
   setHeaders({
-    //'Cache-Control': `public, max-age=${60 * 60 * 24 * 7} no-cache`, // 7 days
     "Cache-Control": "public, must-revalidate",
     "Expires": new Date(Date.now() + 60 * 60 * 24 * 7 * 1e3).toUTCString()
   });
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.D_HUzelG.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.DMcBTYY0.js","_app/immutable/chunks/entry.ROUS9vh2.js"];
+export const imports = ["_app/immutable/nodes/1.CiO8wgZA.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.DMcBTYY0.js","_app/immutable/chunks/entry.B1sIxLZ4.js"];
 export const stylesheets = [];
 export const fonts = [];
